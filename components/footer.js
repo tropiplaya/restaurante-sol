3873c1e0265411ea8b0b91fdba65b05e6c20dbc7
tropiplaya <66205065+tropiplaya@users.noreply.github.com>
2024-10-05 23:04:01 +0200

    feat: v2.0.1 grid size

--- components/footer.js
+++ components/footer.js
@@ -5,7 +5,7 @@ footerTemplate.innerHTML = `
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
   <link rel="stylesheet" href="assets/css/style.css">
 
-  <footer id="footer" class="d-flex flex-row">
+  <footer id="footer" class="d-flex flex-row mt-5">
     <div class="container">
         <div class="row">
             <div class="col-xs-12 col-md-6 px-5">
